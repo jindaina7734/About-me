cc42868e18dc16eb24041d4b5dedb44e20324b28
Nelir <61261926+jindaina7734@users.noreply.github.com>
2023-06-02 22:09:30 +0700

    Create simple_interest.sh

---
+++ simple_interest.sh
@@ -0,0 +1,17 @@
+Simple interest is a method of calculating interest that only takes into account the principal amount and the interest rate. The formula for simple interest is:
+
+SI = P x R x T
+
+
+Simple interest is a method of calculating interest that only takes into account the principal amount and the interest rate. The formula for simple interest is:
+
+Code snippet
+SI = P x R x T
+Use code with caution. Learn more
+Where:
+
+SI = Simple interest
+P = Principal amount
+R = Interest rate
+T = Time period
+For example, if you borrow $100 at an interest rate of 5% for 1 year, you will pay $5 in simple interest.
